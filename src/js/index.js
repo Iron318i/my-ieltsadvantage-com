94885ae7bc68da8b45c3dbd51834a2fc234f1479
Iron318i <iron318i@gmail.com>
2020-06-01 13:35:31 +0300

    13-30

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
 +function ($) {
+    $(document).ready(function () {
+	$(window).on("scroll", function () {
+	    var fromTop = $(window).scrollTop();
+	    $('body').toggleClass("down", (fromTop > 90));
+	});
+    });
 }(jQuery);
